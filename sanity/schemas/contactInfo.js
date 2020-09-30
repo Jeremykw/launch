c70c4a5c188890095662d8be5c30ad6d638d0f6c
Jeremy Bissonnette <jeremy@kwmassage.com>
2020-09-30 10:10:27 -0400

    sanity schema

---
+++ sanity/schemas/contactInfo.js
@@ -0,0 +1,22 @@
+export default {
+  name: 'contactInfo',
+  title: 'Contact Information',
+  type: 'document',
+  fields: [
+    {
+      name: 'address',
+      title: 'Address',
+      type: 'text',
+    },
+    {
+      name: 'phone',
+      title: 'Phone',
+      type: 'number',
+    },
+    {
+      name: 'email',
+      title: 'Email',
+      type: 'string',
+    },
+  ],
+};
---
+++ sanity/schemas/post.js
@@ -0,0 +1,73 @@
+import category from './category';
+
+export default {
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'category',
+      title: 'Category',
+      type: 'reference',
+      to: { type: 'category' },
+    },
+    {
+      name: 'author',
+      title: 'Author',
+      type: 'reference',
+      to: { type: 'author' },
+    },
+    {
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent',
+    },
+    {
+      name: 'caption',
+      title: 'Caption',
+      type: 'text',
+      options: {
+        maxLength: 100,
+      },
+    },
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      author: 'author.name',
+      media: 'mainImage',
+    },
+    prepare(selection) {
+      const { author } = selection;
+      return { ...selection, subtitle: author && `by ${author}` };
+    },
+  },
+};
